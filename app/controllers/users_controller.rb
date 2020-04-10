b76151297ef3791e74388e7de368b7cde2c658b4
Carlos Anriquez <canriquez@gmail.com>
2020-04-10 17:16:56 -0300

    changing to mass assigment using user_params method to permit specific fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
       render :new
     end
 
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
