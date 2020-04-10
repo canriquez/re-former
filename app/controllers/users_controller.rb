c266a502b8190562fe73fa0ced466cbe4edb750a
Carlos Anriquez <canriquez@gmail.com>
2020-04-10 17:39:47 -0300

    refactor form to use form_tag and form_helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.permit(:username, :email, :password)
   end
 
 end
